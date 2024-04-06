22dcb64be575a09908729e4100fb9ac4f1391b40
leakedweights <vkristof20@protonmail.com>
2024-04-06 11:31:11 +0200

    added description and timing

--- dwavexp/tracked.py
+++ dwavexp/tracked.py
@@ -1,6 +1,7 @@
 
 from dataclasses import dataclass
 import inspect
+import time
 from typing import Any, Optional, Sequence
 
 AgentParam = [Sequence | Any]
@@ -8,7 +9,7 @@ AgentParam = [Sequence | Any]
 
 @dataclass
 class SolverConfig:
-    pass
+    track_runtimes: bool = True
 
 
 @dataclass
@@ -17,11 +18,25 @@ class AgentConfig(SolverConfig):
 
 
 class TrackedSolver:
+
+    """
+    Solver with WandB experiment-tracking.
+
+    Usage:
+    ```
+    config = SolverConfig(...)
+    solver = MockSolver(...)
+    solver = TrackedSolver(solver, config)
+
+    solver.my_method(x)
+    ```
+    """
+
     def __init__(self, solver: Any, config: Optional[SolverConfig] = None):
-        if config is not None:
-            self.tracked_methods = []
-        else:
-            self.tracked_methods = self.get_solver_methods(solver)
+        self._dwave_solver = solver
+        self.config = config or SolverConfig()
+
+        self._tracked_methods = self.get_solver_methods(solver)
 
     def auth(self, wandb):
         pass
@@ -29,23 +44,33 @@ class TrackedSolver:
     @staticmethod
     def get_solver_methods(solver: Any):
         methods = [member[0] for member in inspect.getmembers(
-            solver, predicate=inspect.isfunction)]
+            solver.__class__, predicate=inspect.isfunction)]
         return methods
 
     def __getattr__(self, name):
-        orig_attr = getattr(self._dwave_solver, name)
+        try:
+            orig_attr = getattr(self._dwave_solver, name)
+        except AttributeError:
+            raise AttributeError(
+                f"{name} not found in {type(self._dwave_solver).__name__}")
 
         if callable(orig_attr) and name in self._tracked_methods:
             def wrapped(*args, **kwargs):
-                print(f"Before calling {name}")
+                start_time = time.time()
 
                 result = orig_attr(*args, **kwargs)
 
-                print(f"After calling {name}")
+                end_time = time.time()
+                elapsed_time_ms = (end_time - start_time) * 1000
+
+                if self.config.track_runtimes:
+                    print(f"Execution time: {elapsed_time_ms:.3f} ms.")
+
                 return result
 
             return wrapped
-        return orig_attr
+        else:
+            return orig_attr
 
 
 class SolverAgent:
